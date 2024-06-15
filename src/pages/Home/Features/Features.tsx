8a7922b36cd81ef4e72fdeff4cf0134510a3601b
Prosper <enweremproper@gmail.com>
2024-06-15 16:18:35 +0100

    improved features section

--- src/pages/Home/Features/Features.tsx
+++ src/pages/Home/Features/Features.tsx
@@ -4,6 +4,8 @@ import GetStarted from "../../../components/utils/GetStarted";
 import PrivacyImage from "./../../../assets/img/privacy_policy.webp";
 import WhyChooseUsImage from "./../../../assets/img/why_choose_us.webp";
 import HowWeDeliverImage from "./../../../assets/img/how_we_deliver.webp";
+import { Tooltip } from "@mui/material";
+import CarouselImages from "../../../components/utils/CarouselImages/CarouselImages";
 function Features() {
   return (
     <div className="features">
@@ -25,30 +27,74 @@ function Features() {
             <GetStarted variant="contained" />
           </div>
         </div>
-        <div className="features__images">
+        <div className="features__images--phone lg:hidden">
+          <CarouselImages />
+        </div>
+        <div className="features__images--desktop hidden lg:block">
           <div>
-            <div>
-              <img src={WhyChooseUsImage} alt="Why Choose Us Image" />
-              <p className="content-hover">
-                <span>Why Choose Us</span>
-              </p>
-            </div>
+            <Tooltip
+              arrow
+              color="primary"
+              placement="left"
+              title={
+                <p className="text-sm">
+                  <b className="text-primary-light">Surrelink</b> stands out for
+                  our specialized focus on helping Nigerians migrate to
+                  established countries. Our expertise in international
+                  relocation and commitment to personalized service ensures you
+                  receive the best support.
+                </p>
+              }
+            >
+              <div>
+                <img src={WhyChooseUsImage} alt="Why Choose Us Image" />
+                <p className="content-hover">
+                  <span>Why Choose Us</span>
+                </p>
+              </div>
+            </Tooltip>
           </div>
           <div>
-            <div>
-              <img src={HowWeDeliverImage} alt="How We Deliver Image" />
-              <p className="content-hover">
-                <span>How We Deliver</span>
-              </p>
-            </div>
+            <Tooltip
+              arrow
+              placement="top"
+              title={
+                <p className="text-sm">
+                  <b className="text-primary-light">Surrelink</b> handles all
+                  travel arrangements, including domestic and international
+                  flights. We partner with reputable airlines for competitive
+                  prices and coordinated, stress-free travel.
+                </p>
+              }
+            >
+              <div>
+                <img src={HowWeDeliverImage} alt="How We Deliver Image" />
+                <p className="content-hover">
+                  <span>How We Deliver</span>
+                </p>
+              </div>
+            </Tooltip>
           </div>
           <div>
-            <div>
-              <img src={PrivacyImage} alt="Privacy Image" />
-              <p className="content-hover">
-                <span>Privacy Policy</span>
-              </p>
-            </div>
+            <Tooltip
+              title={
+                <p className="text-sm">
+                  At <b className="text-primary-light">Surrelink</b>, we are
+                  committed to providing reliable services. If the migration
+                  process does not go as expected due to our fault, we offer a
+                  full refund. We prioritize your trust and peace of mind.
+                </p>
+              }
+              arrow
+              placement="top"
+            >
+              <div>
+                <img src={PrivacyImage} alt="Privacy Image" />
+                <p className="content-hover">
+                  <span>Refund Policy</span>
+                </p>
+              </div>
+            </Tooltip>
           </div>
         </div>
       </div>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,12 @@
 import { Button, Container, useTheme } from "@mui/material";
 import Hero from "./Hero/Hero";
 import Features from "./Features/Features";
-
+import "./Home.scss";
 export default function Home() {
   const theme = useTheme();
   // console.log(theme.palette.primary.dark);
   return (
-    <Container>
+    <Container className="home">
       <Hero />
       <Features />
     </Container>
